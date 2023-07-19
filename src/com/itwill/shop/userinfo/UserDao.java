c31cad31a6f6bc3d6960892332d619ef118d11ae
silvia32849 <silvia32849@naver.com>
2023-07-19 17:46:55 +0900

    ..

--- src/com/itwill/shop/userinfo/UserDao.java
+++ src/com/itwill/shop/userinfo/UserDao.java
@@ -1,5 +1,21 @@
 package com.itwill.shop.userinfo;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import com.itwill.shop.common.DataSource;
+
 public class UserDao {
 
+	private DataSource dataSource;
+	public UserDao() throws Exception {
+		dataSource = new DataSource();
+	}
+	/*
+	 * insert
+	 */
+	public int insert() throws Exception {
+		Connection con = dataSource.getConnection();;
+		PreparedStatement pstmt = con.prepareStatement(UserSQl.)
+	}
 }

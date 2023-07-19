b1826496481e80566b3140fb7afa922c5c0384d3
ggwanghun <ggwanghun@gmail.com>
2023-07-19 18:35:34 +0900

    ..

--- 산출물/03.테이블정의서/product_insert.sql
+++ 산출물/03.테이블정의서/product_insert.sql
@@ -4,3 +4,15 @@ insert into product(product_no, product_category, product_name, product_image, p
 			values(0, '스포츠', '세모발에서 네모발로', 'images/nothing.jpg', 0);
 insert into product(product_no, product_category, product_name, product_image, product_read_count)
 			values(0, '스포츠', '세모발에서 네모발로', 'images/nothing.jpg', 0);
+insert into product(product_no, product_category, product_name, product_image, product_read_count)
+			values(7, '요리', '포실포실 마들렌 만들기', 'images/nothing.jpg', 0);
+insert into product(product_no, product_category, product_name, product_image, product_read_count)
+			values(8, '요리', '일요일은 내가 짜파게티 요리사', 'images/nothing.jpg', 0);
+insert into product(product_no, product_category, product_name, product_image, product_read_count)
+			values(9, '요리', '세모발에서 네모발로', 'images/nothing.jpg', 0);
+			insert into product(product_no, product_category, product_name, product_image, product_read_count)
+			values(10, '미술', '초상화 그리기', 'images/nothing.jpg', 0);
+insert into product(product_no, product_category, product_name, product_image, product_read_count)
+			values(11, '미술', '조각상 만들기', 'images/nothing.jpg', 0);
+insert into product(product_no, product_category, product_name, product_image, product_read_count)
+			values(12, '미술', '수묵화 그리기', 'images/nothing.jpg', 0);

89d353c911c272b1c70cedac41d921d03157d6a4
82109 <82109@LAPTOP-73C20BQT>
2023-07-19 19:28:47 +0900

    ..

--- src/com/itwill/shop/userinfo/User.java
+++ src/com/itwill/shop/userinfo/User.java
@@ -1,29 +1,47 @@
 package com.itwill.shop.userinfo;
 
 public class User {
+	
 	/*
-	이름       널?       유형            
-	-------- -------- ------------- 
-	USERID   NOT NULL VARCHAR2(100) 
-	PASSWORD          VARCHAR2(100) 
-	NAME              VARCHAR2(100) 
-	EMAIL             VARCHAR2(100) 
-	*/
+	이름              널?       유형            
+			--------------- -------- ------------- 
+			USER_ID         NOT NULL VARCHAR2(100) 
+			USER_PW                  VARCHAR2(100) 
+			USER_PW_CHECK            VARCHAR2(100) 
+			USER_NAME                VARCHAR2(100) 
+			USER_EMAIL               VARCHAR2(100) 
+			USER_EMAIL_SEND          VARCHAR2(100) 
+			USER_JUMIN               VARCHAR2(100) 
+			USER_PHONE               VARCHAR2(100) 
+			USER_GENDER              VARCHAR2(100) 
+    */
+	
 	private String userId;
-	private String userPassword;
+	private String userPw;
+	private String userPwCheck;
 	private String userName;
 	private String userEmail;
+	private String userEmailSend;
+	private String userJumin;
+	private String userPhone;
+	private String userGender;
 	
 	public User() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public User(String userId, String userPassword, String userName, String userEmail) {
+	public User(String userId, String userPw, String userPwCheck, String userName, String userEmail,
+			String userEmailSend, String userJumin, String userPhone, String userGender) {
 		super();
 		this.userId = userId;
-		this.userPassword = userPassword;
+		this.userPw = userPw;
+		this.userPwCheck = userPwCheck;
 		this.userName = userName;
 		this.userEmail = userEmail;
+		this.userEmailSend = userEmailSend;
+		this.userJumin = userJumin;
+		this.userPhone = userPhone;
+		this.userGender = userGender;
 	}
 
 	public String getUserId() {
@@ -34,12 +52,20 @@ public class User {
 		this.userId = userId;
 	}
 
-	public String getUserPassword() {
-		return userPassword;
+	public String getUserPw() {
+		return userPw;
 	}
 
-	public void setUserPassword(String userPassword) {
-		this.userPassword = userPassword;
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+
+	public String getUserPwCheck() {
+		return userPwCheck;
+	}
+
+	public void setUserPwCheck(String userPwCheck) {
+		this.userPwCheck = userPwCheck;
 	}
 
 	public String getUserName() {
@@ -58,11 +84,48 @@ public class User {
 		this.userEmail = userEmail;
 	}
 
+	public String getUserEmailSend() {
+		return userEmailSend;
+	}
+
+	public void setUserEmailSend(String userEmailSend) {
+		this.userEmailSend = userEmailSend;
+	}
+
+	public String getUserJumin() {
+		return userJumin;
+	}
+
+	public void setUserJumin(String userJumin) {
+		this.userJumin = userJumin;
+	}
+
+	public String getUserPhone() {
+		return userPhone;
+	}
+
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+
+	public String getUserGender() {
+		return userGender;
+	}
+
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+
 	@Override
 	public String toString() {
-		return "User [userId=" + userId + ", userPassword=" + userPassword + ", userName=" + userName + ", userEmail="
-				+ userEmail + "]\n";
+		return "User [userId=" + userId + ", userPw=" + userPw + ", userPwCheck=" + userPwCheck + ", userName="
+				+ userName + ", userEmail=" + userEmail + ", userEmailSend=" + userEmailSend + ", userJumin="
+				+ userJumin + ", userPhone=" + userPhone + ", userGender=" + userGender + "]";
 	}
+
+
+	
+	
 	
 	
 }

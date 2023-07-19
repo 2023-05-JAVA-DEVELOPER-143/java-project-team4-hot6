f606cb40ded904114bef2cef28d5e46f8611ed1b
qlsjsek2 <qlsjsek2@naver.com>
2023-07-19 20:54:07 +0900

    cart~

--- 산출물/03.테이블정의서/cart_select_update_delete.sql
+++ 산출물/03.테이블정의서/cart_select_update_delete.sql
@@ -1,23 +1,17 @@
---guard1 멤버한사람의  카트에 제품번호 존재여부
-select count(*) as p_count from cart c join userinfo u on c.user_id=u.user_id where u.user_id='guard1' and c.product_no=1;
+--select
+select * from cart where cart_no=2;
+select * from cart c join product p on c.product_no=p.product_no where cart_no=2;
 
---guard1 멤버한사람의 카트아이템리스트
-select * 
-from cart c join userinfo u
-on c.user_id=u.user_id 
-join product p 
-on p.product_no=c.product_no
-where u.user_id='guard1';
+--update
+--상품 수량 증가
+update cart set cart_qty=cart_qty+1 where cart_no=2;
+--상품 수량 감소
+update cart set cart_qty=cart_qty-1 where cart_no=2;
 
---카트아이템 1개의 제품정보(guard1 멤버한사람)
-select * from cart c join product p on c.product_no=p.product_no where cart_no=1 ;
-
---guard1 카트에 있는 1번제품의 수량증가
-update cart set cart_qty=cart_qty+1 where user_id='guard1' and p_no=1;
---guard1 카트에 있는 cart_no 1번의 수량3개 수정
-update cart set cart_qty=3 where cart_no=1
-
---guard1님 카트아이템1개삭제
-delete from cart where cart_no=1;
---guard1님 카트아이템모두삭제
+--delete
+--cart 아이템 1개 삭제
+delete from cart where cart_no=2;
+--cart 아이템 모두삭제
 delete from cart where user_id='guard1';
+
+

e093dc7d96480393126e3e7dc94c138b080f178e
ggwanghun <ggwanghun@gmail.com>
2023-07-20 14:52:27 +0900

    ..

--- src/com/itwill/shop/product/ProductService.java
+++ src/com/itwill/shop/product/ProductService.java
@@ -2,31 +2,42 @@ package com.itwill.shop.product;
 
 import java.util.List;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 public class ProductService {
 	private ProductDao productDao;
 	public ProductService() throws Exception{
 		productDao=new ProductDao();
-		
 	}
 	/*
 	 * product 전체출력
 	 */
 	//public List<Product> productList() throws Exception{
-		
+	public List<Product> productList() throws Exception{
+		return productDao.findAll();
 	}
 	/*
 	 *  product 번호에 따른 출력
 	 */
-	
+	public Product findByPrimaryKey() throws Exception{
+		return productDao.findByPrimaryKey(0);
+	}
 	/*
 	 *  product 이름에 따른 검색
 	 */
-	
+	public Product findByKeyword() throws Exception{
+		return productDao.findByKeyword(null);
+	}
 	/*
 	 *  product 카테고리에 따른 검색
 	 */
-	
+	public Product findByCategory() throws Exception{
+		return productDao.findByCategory(null);
+	}
 	/*
 	 *  product 조회수 증가 기능
 	 */
-	
+	public Product updateByReadCount() throws Exception{
+		//return pro
+	}
+}

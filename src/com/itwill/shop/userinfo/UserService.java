f83f0115afb0852c429519174d6dffc629556a87
7074220 <7074220@naver.com>
2023-07-20 14:09:03 +0900

    ..

--- src/com/itwill/shop/userinfo/UserService.java
+++ src/com/itwill/shop/userinfo/UserService.java
@@ -1,5 +1,31 @@
 package com.itwill.shop.userinfo;
 
-public class UserService {
 
-}
+public class UserService {
+	
+	private UserDao userDao;
+	public UserService() throws Exception {
+		userDao=new UserDao();
+	}
+	/*
+	 * 회원가입
+	 */
+	public boolean addUser(User newUser) {
+		boolean isSuccess=false;
+
+	/*
+	 * 아이디존재여부 
+	 */
+	User findUser=userDao.findByUserId(newUser.getUserId());
+	if(findUser==null) {
+		int rowCount=userDao.insert(newUser);
+		isSuccess=true;//아이디 중복 안될시에 
+		
+	}else {
+		isSuccess=false;
+	}
+	
+		return isSuccess;
+		
+	}	
+	

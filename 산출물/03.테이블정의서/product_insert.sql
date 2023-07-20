cfbbee6041a4ad6deac82638fbfc63b1c01c0b36
차경진 <ckrudwls7@naver.com>
2023-07-20 15:59:56 +0900

    ...

--- 산출물/03.테이블정의서/product_insert.sql
+++ 산출물/03.테이블정의서/product_insert.sql
@@ -1,26 +1,26 @@
 
 insert into product(product_no, product_start_date, product_category, product_name, product_price, product_detail, product_image, product_read_count)
-			values(product_product_no_SEQ.nextval, '2023-07-28', '플라워', 50000, '[종로/인사동] 나만의 작은 정원 테라리움 원데이클래스', '저와 함께 촉촉한 흙을 만지며 힐링해보세요!', 'images/nothing.jpg', 0);
+			values(product_product_no_SEQ.nextval, '2023-07-28', '플라워', '[종로/인사동] 나만의 작은 정원 테라리움 원데이클래스', 50000,  '저와 함께 촉촉한 흙을 만지며 힐링해보세요!', 'images/nothing.jpg', 0);
 insert into product(product_no, product_start_date, product_category, product_name, product_price, product_detail, product_image, product_read_count)
-			values(product_product_no_SEQ.nextval, '2023-08-05', '플라워', 70000, '[망원] 새벽정원 화병꽂이 클래스(화병포함)', '망리단길에 위치한 플라워스튜디오 새벽정원입니다.', 'images/nothing.jpg', 0);
+			values(product_product_no_SEQ.nextval, '2023-08-05', '플라워', '[망원] 새벽정원 화병꽂이 클래스(화병포함)', 70000, '망리단길에 위치한 플라워스튜디오 새벽정원입니다.', 'images/nothing.jpg', 0);
 insert into product(product_no, product_start_date, product_category, product_name, product_price, product_detail, product_image, product_read_count)
-			values(product_product_no_SEQ.nextval, '2023-08-09', '플라워', 80000, '[송파] 초보자들도 쉽게 배울 수 있는 꽃다발', '함께 예쁜 꽃 만들어가는 힐링클래스가 되었으면 좋겠어요! ', 'images/nothing.jpg', 0);			
+			values(product_product_no_SEQ.nextval, '2023-08-09', '플라워', '[송파] 초보자들도 쉽게 배울 수 있는 꽃다발', 80000,  '함께 예쁜 꽃 만들어가는 힐링클래스가 되었으면 좋겠어요! ', 'images/nothing.jpg', 0);			
 insert into product(product_no, product_start_date, product_category, product_name, product_price, product_detail, product_image, product_read_count)
-			values(product_product_no_SEQ.nextval, '2023-08-12', '수공예', 90000, '[원데이/성수] 색에 매력에 빠져드는 마블 물레 도자기 원데이클래스','단 하나뿐인 나만의 도자기 그릇을 만들어 보세요:)','images/nothing.jpg', 0);
+			values(product_product_no_SEQ.nextval, '2023-08-12', '수공예', '[원데이/성수] 색에 매력에 빠져드는 마블 물레 도자기 원데이클래스', 90000, '단 하나뿐인 나만의 도자기 그릇을 만들어 보세요:)','images/nothing.jpg', 0);
 insert into product(product_no, product_start_date, product_category, product_name, product_price, product_detail, product_image, product_read_count)
-			values(product_product_no_SEQ.nextval, '2023-08-05', '수공예', 100000, '[연남] 감각적인 시그니처 향수 제작 A Class 퍼퓸', '상상속에 존재하는 나만의 시그니처 향수 만들기','images/nothing.jpg', 0);
+			values(product_product_no_SEQ.nextval, '2023-08-05', '수공예', '[연남] 감각적인 시그니처 향수 제작 A Class 퍼퓸', 100000,  '상상속에 존재하는 나만의 시그니처 향수 만들기','images/nothing.jpg', 0);
 insert into product(product_no, product_start_date, product_category, product_name, product_price, product_detail, product_image, product_read_count)
-			values(product_product_no_SEQ.nextval, '2023-08-10', '수공예', 110000, '[연남] 가죽 공예 데일리 카드 지갑 만들기 원데이 클래스', '나만의 이니셜 각인도 해드려요!', 'images/nothing.jpg', 0);		
+			values(product_product_no_SEQ.nextval, '2023-08-10', '수공예', '[연남] 가죽 공예 데일리 카드 지갑 만들기 원데이 클래스', 110000,  '나만의 이니셜 각인도 해드려요!', 'images/nothing.jpg', 0);		
 insert into product(product_no, product_start_date, product_category, product_name, product_price, product_detail, product_image, product_read_count)
-			values(product_product_no_SEQ.nextval, '2023-08-08', '요리', 130000, '[종로/서촌] 고급재료 듬뿍 피낭시에 구움과자','고급재료로 만드는 피낭시에입니다.','images/nothing.jpg', 0);
+			values(product_product_no_SEQ.nextval, '2023-08-08', '요리', '[종로/서촌] 고급재료 듬뿍 피낭시에 구움과자','고급재료로 만드는 피낭시에입니다.', 130000, 'images/nothing.jpg', 0);
 insert into product(product_no, product_start_date, product_category, product_name, product_price, product_detail, product_image, product_read_count)
-			values(product_product_no_SEQ.nextval, '2023-08-05', '요리', 140000, '[망원] 다양한 베이킹', '원하는 빵을 만들 수 있습니다!','images/nothing.jpg', 0);
+			values(product_product_no_SEQ.nextval, '2023-08-05', '요리','[망원] 다양한 베이킹', 140000, '원하는 빵을 만들 수 있습니다!','images/nothing.jpg', 0);
 insert into product(product_no, product_start_date, product_category, product_name, product_price, product_detail, product_image, product_read_count)
-			values(product_product_no_SEQ.nextval, '2023-08-10', '요리', 230000, '[마포] 포실포실 크림마들렌 8개 만들기, 미니 플라워 바구니 원데이클래스', '크림마들렌이 8개입니다.', 'images/nothing.jpg', 0);		
+			values(product_product_no_SEQ.nextval, '2023-08-10', '요리', '[마포] 포실포실 크림마들렌 8개 만들기, 미니 플라워 바구니 원데이클래스', 230000,  '크림마들렌이 8개입니다.', 'images/nothing.jpg', 0);		
 insert into product(product_no, product_start_date, product_category, product_name, product_price, product_detail, product_image, product_read_count)
-			values(product_product_no_SEQ.nextval, '2023-08-23', '미술', 300000, '[마포] 초보자도 쉽고 즐겁게 하는 브릭베어 아크릴화', '아크릴로 만듭니다.','images/nothing.jpg', 0);
+			values(product_product_no_SEQ.nextval, '2023-08-23', '미술', '[마포] 초보자도 쉽고 즐겁게 하는 브릭베어 아크릴화', 300000,  '아크릴로 만듭니다.','images/nothing.jpg', 0);
 insert into product(product_no, product_start_date, product_category, product_name, product_price, product_detail, product_image, product_read_count)
-			values(product_product_no_SEQ.nextval, '2023-08-09', '미술', 80000, '[서울] 하루만에 나만의 이모티콘 만들기', '용돈벌이가 쏠쏠합니다.','images/nothing.jpg', 0);			
+			values(product_product_no_SEQ.nextval, '2023-08-09', '미술', '[서울] 하루만에 나만의 이모티콘 만들기', 80000,  '용돈벌이가 쏠쏠합니다.','images/nothing.jpg', 0);			
 insert into product(product_no, product_start_date, product_category, product_name, product_price, product_detail, product_image, product_read_count)
-			values(product_product_no_SEQ.nextval, '2023-08-09', '미술', 150000, '[합정] 나이프화, 아크릴화','나이프화입니다.', 'images/nothing.jpg', 0);	
+			values(product_product_no_SEQ.nextval, '2023-08-09', '미술', '[합정] 나이프화, 아크릴화', 150000, '나이프화입니다.', 'images/nothing.jpg', 0);	
 

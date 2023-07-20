250544ee8ce1af2d0d497ff52d84b882b440e133
ggwanghun <ggwanghun@gmail.com>
2023-07-20 15:44:04 +0900

    ..

--- src/com/itwill/shop/test/ProductServiceTestMain.java
+++ src/com/itwill/shop/test/ProductServiceTestMain.java
@@ -7,15 +7,14 @@ public class ProductServiceTestMain {
 		
 		ProductService productService=new ProductService();
 		System.out.println("1.selectAll:");
-		System.out.println("  "+productService.findAll());
+		System.out.println("  "+productService.productList());
 		System.out.println("2.findByPrimaryKey");
-		System.out.println(productService.findByPrimaryKey(1));
+		System.out.println(productService.productDetail(1));
 		System.out.println("3.findByKeyword");
-		System.out.println(productService.findByKeyword("[망원] 다양한 베이킹"));
+		System.out.println(productService.productSearch("베이킹"));
 		System.out.println("4.findByCategory");
-		System.out.println(productService.findByCategory("미술"));
+		System.out.println(productService.productCategorySearch("미술"));
 		System.out.println("5.updateByReadCount");
-		//productDao.updateByReadCount(0);
-		System.out.println(productService.updateByReadCount(0));
+		System.out.println(productService.productReadCount(1)); 
 	}
 }

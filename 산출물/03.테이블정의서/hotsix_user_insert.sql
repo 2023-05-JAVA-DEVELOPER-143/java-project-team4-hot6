38633704db021f94be6a82669d1587b435a8f56e
silvia32849 <silvia32849@naver.com>
2023-07-20 13:19:03 +0900

    ..

--- 산출물/03.테이블정의서/hotsix_user_insert.sql
+++ 산출물/03.테이블정의서/hotsix_user_insert.sql
@@ -1,7 +1,7 @@
 insert into userinfo(user_id, user_pw, user_pw_check, user_name, user_email, user_email_send, user_jumin, user_phone, user_gender)
 			values('guard1', '1111', '1111', '경호원', 'guard1@naver.com', 'T', '750719', '010-1234-5678', '남성');
 insert into userinfo(user_id, user_pw, user_pw_check, user_name, user_email, user_email_send, user_jumin, user_phone, user_gender)
-			values('guard2', '2222', '2222', '경호투', 'guard2@naver.com', 'F', '850719', '010-5678-1234', 여성');
+			values('guard2', '2222', '2222', '경호투', 'guard2@naver.com', 'F', '850719', '010-5678-1234', '여성');
 insert into userinfo(user_id, user_pw, user_pw_check, user_name, user_email, user_email_send, user_jumin, user_phone, user_gender)
 			values('guard3', '3333', '3333', '경호삼', 'guard2@naver.com', 'T', '950719', '010-3456-9587', '여성');
 insert into userinfo(user_id,user_pw,user_pw_check,user_name, user_email, user_email_send, user_jumin, user_phone, user_gender)

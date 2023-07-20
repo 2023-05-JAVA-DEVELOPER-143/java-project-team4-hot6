2300de916de088700ee06f8bb73df1567b2a1d35
차경진 <ckrudwls7@naver.com>
2023-07-20 15:10:07 +0900

    Merge branch 'master' of https://github.com/2023-05-JAVA-DEVELOPER-143/java-project-team4-hot6

--- src/com/itwill/shop/product/ProductService.java
+++ src/com/itwill/shop/product/ProductService.java
@@ -11,22 +11,33 @@ public class ProductService {
 	/*
 	 * product 전체출력
 	 */
-	//public List<Product> productList() throws Exception{
-		
+	public List<Product> productList() throws Exception{
+		return productDao.findAll();
 	}
 	/*
 	 *  product 번호에 따른 출력
 	 */
-	
+	public Product productDetail(int product_no) throws Exception{
+		return productDao.findByPrimaryKey(product_no);
+	}
 	/*
 	 *  product 이름에 따른 검색
 	 */
+	public Product productSearch(String product_name) throws Exception{
+		return productDao.findByKeyword(product_name);
+	}
 	
 	/*
 	 *  product 카테고리에 따른 검색
 	 */
+	public Product productCategorySearch(String product_category) throws Exception{
+		return productDao.findByCategory(product_category);
+	}
 	
 	/*
 	 *  product 조회수 증가 기능
 	 */
-	
+	private int productReadCount(int product_no) throws Exception{
+		return productDao.updateByReadCount(product_no);
+	}
+}

3705a66fd8d62b7d2d8f6309f350347f297d4387
Roco-Lee <aowlrtmd@gmail.com>
2023-07-20 14:16:06 +0900

    order Dao

--- src/com/itwill/shop/order/OrderService.java
+++ src/com/itwill/shop/order/OrderService.java
@@ -1,5 +1,46 @@
 package com.itwill.shop.order;
 
-public class OrderService {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.itwill.shop.cart.Cart;
+import com.itwill.shop.cart.CartDao;
+import com.itwill.shop.product.ProductDao;
+
+public class OrderService {
+	private OrderDao orderDao;
+	private ProductDao productDao;
+	private CartDao cartDao;
+	
+	public OrderService(OrderDao orderDao, ProductDao productDao, CartDao cartDao) {
+		this.orderDao = orderDao;
+		this.productDao = productDao;
+		this.cartDao = cartDao;
+	}
+
+	// 결제버튼 (cart -> order)
+	public int create(String selectUserId) throws Exception{
+		List<Cart> cartList=cartDao.findByUserId(selectUserId);
+		ArrayList<OrderItem> orderItemList=new ArrayList<OrderItem>();
+		int o_tot_price=0;
+		int oi_tot_count=0;
+		for (Cart cart : cartList) {
+			OrderItem orderItem=new OrderItem(0,cart.getCart_qty(),0, cart.getProduct());
+			orderItemList.add(orderItem);
+			o_tot_price+=orderItem.getOi_qty()*orderItem.getProduct().getP_price();
+			oi_tot_count+=orderItem.getOi_qty();
+		}
+		String o_desc = orderItemList.get(0).getProduct().getP_name()+"외 "+(oi_tot_count-1)+" 개";
+		Order newOrder=new Order(0,o_desc, null, o_tot_price, selectUserId,orderItemList);
+		orderDao.insert(newOrder);
+		cartDao.deleteByUserId(selectUserId);
+		return 0;
+	}  
+	
+	// 주문내역 (결제완료 조회)
+	public ArrayList<Order> list(String selectUserId) throws Exception{
+		return orderDao.findByUserId(selectUserId);
+	}
+	
+	
 }
--- src/com/itwill/shop/test/OrderDaoTestMain.java
+++ src/com/itwill/shop/test/OrderDaoTestMain.java
@@ -1,7 +1,34 @@
 package com.itwill.shop.test;
 
+import java.sql.Date;
+import java.util.ArrayList;
+
+import com.itwill.shop.order.Order;
+import com.itwill.shop.order.OrderDao;
+import com.itwill.shop.order.OrderItem;
+import com.itwill.shop.product.Product;
+import com.itwill.shop.product.ProductDao;
+
 public class OrderDaoTestMain {
 	public static void main(String[] args) throws Exception  {
+		OrderDao orderDao = new OrderDao();
 		
+		// 아이디로 결제내역 조회
+		System.out.println(orderDao.findByUserId("guard1"));
+
+		// 상품에서 직접 주문
+		int p_qty=1;
+		int p_no=1;
+		ProductDao productDao=new ProductDao();
+		
+		
+		ArrayList<OrderItem> jumunDetailList=new ArrayList<OrderItem>();
+		jumunDetailList.add(new OrderItem(0, p_qty, new Product(p_no, new Date(0), "미술", "축구", "세모발", "/images.jpg", 0), p_no));
+		Order newJumun=new Order(0, "구매자", "번호", 5000, new Date(0), "guard1", jumunDetailList);
+		
+		System.out.println(orderDao.insert(newJumun));
+		
+		// 장바구니에서 결제
+	
 	}
 }

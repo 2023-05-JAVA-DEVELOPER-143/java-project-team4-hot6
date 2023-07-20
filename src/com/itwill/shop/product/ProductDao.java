4349e5ee944701c14a9f0149218bcd22596d76d6
ggwanghun <ggwanghun@gmail.com>
2023-07-20 12:03:09 +0900

    ..

--- src/com/itwill/shop/product/ProductDao.java
+++ src/com/itwill/shop/product/ProductDao.java
@@ -26,9 +26,10 @@ public class ProductDao {
 		while(rs.next()) {
 			Product product=new Product(
 							rs.getInt("product_no"),
-							rs.getString("product_start_date"),
+							rs.getDate("product_start_date"),
 							rs.getString("product_category"),
 							rs.getString("product_name"),
+							rs.getString("product_detail"),
 							rs.getString("product_image"),
 							rs.getInt("product_read_count"));
 			productList.add(product);
@@ -47,25 +48,71 @@ public class ProductDao {
 		if(rs.next()) {
 			product= 
 					new Product(
-							rs.getInt(product_no);
-							rs.
-					)
+							rs.getInt("product_no"),
+							rs.getDate("product_start_date"),
+							rs.getString("product_category"),
+							rs.getString("product_name"),
+							rs.getString("product_detail"),
+							rs.getString("product_image"),
+							rs.getInt("product_read_count"));
 		}
 		return product;
 	}
 /*
  *  product 이름에 따른 검색
  */
-	
-/*
- *  product 이름에 따른 검색
- */
-	
+	public Product findByProductName(String product_name) throws Exception{
+		Product product=null;
+		Connection con=dataSource.getConnection();
+		PreparedStatement pstmt=con.prepareStatement(ProductSQL.PRODUCT_SELECT_BY_NAME);
+		pstmt.setString(1, product_name);
+		ResultSet rs=pstmt.executeQuery();
+		if(rs.next()) {
+			product= 
+					new Product(
+							rs.getInt("product_no"),
+							rs.getDate("product_start_date"),
+							rs.getString("product_category"),
+							rs.getString("product_name"),
+							rs.getString("product_detail"),
+							rs.getString("product_image"),
+							rs.getInt("product_read_count"));
+		}
+		return product;
+	}
 /*
  *  product 카테고리에 따른 검색
  */
-	
+	public Product findByProductCategory(String product_category) throws Exception{
+		Product product=null;
+		Connection con=dataSource.getConnection();
+		PreparedStatement pstmt=con.prepareStatement(product_category);
+		pstmt.setString(1, product_category);
+		ResultSet rs=pstmt.executeQuery();
+		if(rs.next()) {
+			product= 
+					new Product(
+							rs.getInt("product_no"),
+							rs.getDate("product_start_date"),
+							rs.getString("product_category"),
+							rs.getString("product_name"),
+							rs.getString("product_detail"),
+							rs.getString("product_image"),
+							rs.getInt("product_read_count"));
+		}
+		return product;
+	}
+		
 /*
  *  product 조회수 증가 기능
  */
+	public int UpdateProductReadCount(int product_no) throws Exception{
+		Connection con=dataSource.getConnection();
+		PreparedStatement pstmt=con.prepareStatement(ProductSQL.PRODUCT_UPDATE_READ_COUNT);
+		pstmt.setInt(1, product_no);
+		int rowCount= 0;
+		rowCount = pstmt.executeUpdate();
+		return rowCount;
+	}
 }
+

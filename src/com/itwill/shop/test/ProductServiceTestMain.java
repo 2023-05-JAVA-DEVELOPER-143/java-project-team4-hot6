7cfd9b90db4df7fa6a00bfcea49924f8b00918f5
ggwanghun <ggwanghun@gmail.com>
2023-07-20 16:41:52 +0900

    ..

--- src/com/itwill/shop/test/ProductServiceTestMain.java
+++ src/com/itwill/shop/test/ProductServiceTestMain.java
@@ -11,10 +11,10 @@ public class ProductServiceTestMain {
 		System.out.println("2.findByPrimaryKey");
 		System.out.println(productService.productDetail(1));
 		System.out.println("3.findByKeyword");
-		System.out.println(productService.productSearch("베이킹"));
+		System.out.println(productService.productSearch("마들렌"));
 		System.out.println("4.findByCategory");
-//		System.out.println(productService.productCategorySearch("미술"));
+		System.out.println(productService.productCategorySearch("미술"));
 		System.out.println("5.updateByReadCount");
-		System.out.println(productService.productReadCount(0)); 
+		System.out.println(productService.productReadCount(2)); 
 	}
 }
